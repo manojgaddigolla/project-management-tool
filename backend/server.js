d89534036ea05dda333dadde501c20cb5221f008
Gaddigolla Manoj <manojgaddigolla@gmail.com>
2026-04-08 19:45:09 +0530

    Backend Foundation

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,20 @@
 require('dotenv').config();
+
+const connectDB = require('./config/db');
+connectDB();
+
 const express = require('express');
 const app = express();
+
+const cors = require('cors');
+app.use(cors());
+
+app.use(express.json());
+
+app.get('/api/health', (req, res) => {
+  res.json({ status: 'UP', message: 'Server is healthy.' });
+});
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
