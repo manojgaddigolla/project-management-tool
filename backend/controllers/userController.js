5bb24f36b4e98c06cb2f496864dfcd321c175d08
Gaddigolla Manoj <manojgaddigolla@gmail.com>
2026-04-11 13:41:59 +0530

    Implement User Registration Controller with JWT

---
+++ backend/controllers/userController.js
@@ -0,0 +1,52 @@
+const { validationResult } = require('express-validator');
+const User = require('../models/User');
+const jwt = require('jsonwebtoken');
+
+const registerUser = async (req, res) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
+
+  const { name, email, password } = req.body;
+
+  try {
+    let user = await User.findOne({ email });
+
+    if (user) {
+      return res.status(400).json({ errors: [{ msg: 'User already exists' }] });
+    }
+
+    user = new User({
+      name,
+      email,
+      password,
+    });
+
+    await user.save();
+
+    const payload = {
+      user: {
+        id: user.id, 
+      },
+    };
+
+    jwt.sign(
+      payload,
+      process.env.JWT_SECRET, 
+      { expiresIn: '7d' }, 
+      (err, token) => {
+        if (err) throw err;
+
+        res.status(201).json({ token });
+      }
+    );
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server error');
+  }
+};
+
+module.exports = {
+  registerUser,
+};
--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,8 +1,7 @@
 const express = require("express");
-
 const router = express.Router();
-
 const { check, validationResult } = require("express-validator");
+const { registerUser } = require("../controllers/userController");
 
 router.post(
   "/register",
@@ -16,15 +15,7 @@ router.post(
       "Please enter a password with 6 or more characters",
     ).isLength({ min: 6 }),
   ],
-  (req, res) => {
-    const errors = validationResult(req);
-
-    if (!errors.isEmpty()) {
-      return res.status(400).json({ errors: errors.array() });
-    }
-
-    res.send("User registration route is working! (Validation passed)");
-  },
+  registerUser
 );
 
 module.exports = router;
