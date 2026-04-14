fecbfc34d61e4d91fc5119eff702b51b3f2474b4
Gaddigolla Manoj <manojgaddigolla@gmail.com>
2026-04-14 21:13:28 +0530

    User Authentication & Authorization API

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -2,7 +2,10 @@ const express = require('express');
 const router = express.Router();
 
 const { check } = require('express-validator');
-const { loginUser } = require('../controllers/userController'); 
+const { loginUser,getAuthenticatedUser } = require('../controllers/userController'); 
+const auth = require('../middleware/auth');
+
+router.get('/', auth, getAuthenticatedUser);
 
 router.post('/login', [
     check('email', 'Please include a valid email').isEmail(),
