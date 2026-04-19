bbd66354b59eba5ca0a8f132def05358b6035608
Gaddigolla Manoj <manojgaddigolla@gmail.com>
2026-04-19 23:25:09 +0530

    Implement Authentication using JWT

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,11 +1,27 @@
-import axios from 'axios';
+import axios from "axios";
+import useAuthStore from "../store/authStore";
 
 const api = axios.create({
-  baseURL: 'http://localhost:5000/api',
+  baseURL: "http://localhost:5000/api",
 
   headers: {
-    'Content-Type': 'application/json',
+    "Content-Type": "application/json",
   },
 });
+
+api.interceptors.request.use(
+  (config) => {
+    const { token } = useAuthStore.getState();
+
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  },
+);
 
 export default api;
